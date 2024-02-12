4255624c0c93e168c40a454cfcae484a4cf33e67
75008Kajal <128771800+75008Kajal@users.noreply.github.com>
2024-02-12 11:33:40 +0530

    er diara

--- ReactHome/homestayreact/src/App.js
+++ ReactHome/homestayreact/src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './home';
+import Login from './login';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState("")
+    
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +24,13 @@ function App() {
         >
           Learn React
         </a>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>} />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+        </Routes>
+      </BrowserRouter>
+    
       </header>
     </div>
   );
